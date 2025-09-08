4abf803ec671fe11a9f3e1c28d5bd45f67526cd0
Rafael Wampfler (U114379) <u114379@mobi.ch>
2025-09-08 11:33:04 +0200

    localstorage, jira test call

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,5 +9,40 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('planningApp');
+
+  private http = inject(HttpClient);
+  private token = '';
+  private url = '';
+
+  aktualisieren() {
+    this.token = localStorage.getItem('token') || '';
+    if (!this.token) {
+      this.token = prompt("Enter your Jira Token") || '';
+      localStorage.setItem('token', this.token);
+    }
+
+    this.url = localStorage.getItem('url') || '';
+    if (!this.url) {
+      this.url = prompt("Enter your Jira URL") || '';
+      localStorage.setItem('url', this.url);
+    }
+
+    this.queryJira(21232);
+  }
+
+  queryJira(sprintId: number) {
+    this.http.get(`${this.url}/rest/api/2/search?jql=sprint=${sprintId}`, {
+      headers: {
+        'Authorization': `Bearer ${this.token}`,
+        'Accept': 'application/json'
+      }
+    }).subscribe({
+      next: (data) => {
+        console.log(data);
+      },
+      error: (error) => {
+        console.error('There was an error!', error);
+      }
+    })
+  }
 }
